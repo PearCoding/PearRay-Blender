989cd6ba3258670b0c0e0d9d1fba7fbb67038b66
PearCoding <pearcoding@gmail.com>
2019-10-19 01:18:05 +0200

    Tiny (maybe not good) fix of camera issue

--- export/camera.py
+++ export/camera.py
@@ -11,20 +11,27 @@ def export_camera(exporter, camera):
     w.write("(camera")
     w.goIn()
 
-    w.write(":name '%s'" % camera.name)
-    w.write(":type 'standard'")
+    aspectW = (camera.data.sensor_width/camera.data.lens)
+    aspectH = aspectW#(camera.data.sensor_height/camera.data.lens)
 
-    if camera.data.type == 'ORTHO':
-        w.write(":projection 'ortho'")
-        w.write(":width %f" % (camera.data.ortho_scale * aspectW))
-        w.write(":height %f" % (camera.data.ortho_scale * aspectH))
+    if scene.render.resolution_x > scene.render.resolution_y:
+        aspectH = aspectH * scene.render.resolution_y / scene.render.resolution_x
     else:
-        w.write(":width %f" % (camera.data.sensor_width/camera.data.lens))
-        w.write(":height %f" % (camera.data.sensor_height/camera.data.lens))
+        aspectW = aspectW * scene.render.resolution_x / scene.render.resolution_y
 
-    w.write(":zoom %f" % camera.data.pearray.zoom)
-    w.write(":fstop %f" % camera.data.pearray.fstop)
-    w.write(":apertureRadius %f" % camera.data.pearray.apertureRadius)
+    w.write(":name '%s'" % camera.name)
+    if camera.data.type == 'ORTHO':
+        w.write(":type 'orthographic'")
+        w.write(":width %f" % (camera.data.ortho_scale*aspectW))
+        w.write(":height %f" % (camera.data.ortho_scale*aspectH))
+    else:
+        w.write(":type 'standard'")
+        w.write(":width %f" % (aspectW))
+        w.write(":height %f" % (aspectH))
+        w.write(":zoom %f" % camera.data.pearray.zoom)
+        w.write(":fstop %f" % camera.data.pearray.fstop)
+        w.write(":apertureRadius %f" % camera.data.pearray.apertureRadius)
+
     w.write(":localDirection [0,0,-1]")
     w.write(":localUp [0,-1,0]")
     w.write(":localRight [1,0,0]")
